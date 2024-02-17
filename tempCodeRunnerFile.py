2fcc18e1cab0539cc5699c99b70879ce6bb3b295
DougFelipe <dougbti2022@gmail.com>
2024-02-16 23:11:48 -0300

    feat: ajuste das páginas e função de upload de arquivos

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,25 @@
+# my_dash_app/index.py
+from dash import Dash
+from dash import dcc
+from dash import html
+from components.header import Header
+from layouts.about import get_about_layout
+from layouts.data_analysis import get_dataAnalysis_layout
+
+from callbacks.callbacks import render_tab_content
+
+from app import app
+
+
+
 app.layout = html.Div(className='main-content', children=[
-    html.Header(className='main-header', children=[
-        html.H1(children='Biorremediation Potential Explorer & Analyzer', className='main-title'),
-        html.A("View on GitHub", href="https://github.com/yourrepo/yourproject", target="_blank",
-               className='github-link')
-    ]),
+    Header(),
     dcc.Tabs(id="tabs", value='tab-about', children=[
         dcc.Tab(label='About', value='tab-about'),
         dcc.Tab(label='Data Analysis', value='tab-data-analysis')
     ], className='main-tabs'),
     html.Div(id='tabs-content', className='tabs-content')
 ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
