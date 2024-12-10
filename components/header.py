dc83a0b5e99abfa07e6a6ab5165b5673eb4494da
DougFelipe <dougbti2022@gmail.com>
2024-12-10 19:20:40 -0300

    refactor(header): reajusta ordem dos liks no header e omite changelog

--- components/header.py
+++ components/header.py
@@ -7,11 +7,12 @@ def Header():
             html.A('BioPExA', href='/about', className='main-title')
         ]),
         html.Div(className='header-right', children=[
-            html.A('BioPExa Features', href='/features', className='header-link'),
-            html.A('Bioremediation', href='/bioremediation', className='header-link'),
-            html.A('Regulatory Agencies', href='/regulatory', className='header-link'),
-            html.A('Changelog', href='/changelog', className='header-link'),
-            html.A('Contact', href='/contact', className='header-link'),
             html.A("Help", href="/help", className="header-link"),
+            html.A('BioPExa Features', href='/features', className='header-link'),
+            html.A('Regulatory Agencies', href='/regulatory', className='header-link'),
+            html.A('Bioremediation', href='/bioremediation', className='header-link'),
+            #html.A('Changelog', href='/changelog', className='header-link'),
+            html.A('Contact', href='/contact', className='header-link'),
+            
         ])
     ])
