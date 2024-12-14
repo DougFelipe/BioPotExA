442b0d9ab3f63bc86f8332ba82e85a2ed22bad15
DougFelipe <dougbti2022@gmail.com>
2024-12-14 20:59:32 -0300

    style(navbar): ajusta o estilo da navbar para dividir os resultados em sections

--- components/navbar.py
+++ components/navbar.py
@@ -1,6 +1,7 @@
 from dash import html
+import dash_bootstrap_components as dbc
 
-# Navbar ajustado com todas as sessões dentro de nav-links
+# Navbar ajustado com todas as sessões como dbc.Cards
 navbar = html.Div(
     [
         # Título da aplicação
@@ -18,48 +19,103 @@ navbar = html.Div(
                     className="navbar-subtitle",  # Classe CSS para o subtítulo
                 ),
 
-                # Links organizados em uma única div "nav-links"
+                # Links organizados em dbc.Cards para 7 sessões
                 html.Div(
                     [
                         # Sessão 1: Data Tables and Database Integration
-                        html.P("1 - Data Tables and Database Integration", className="navbar-section-title"),
-                        html.A("Main Results Table", href="#main-results-table", className="nav-link"),
-                        html.A("Results Table (hadegDB)", href="#hadeg-results-table", className="nav-link"),
-                        html.A("Results Table (ToxCSM)", href="#toxcsm-results-table", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("1 - Data Tables and Database Integration", className="card-title"),
+                                    html.A("BioRemPP Results Table", href="#main-results-table", className="nav-link"),
+                                    html.A("HADEG Results Table", href="#hadeg-results-table", className="nav-link"),
+                                    html.A("ToxCSM Results Table", href="#toxcsm-results-table", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
 
                         # Sessão 2: Gene and Metabolic Pathway Analysis
-                        html.P("2 - Gene and Metabolic Pathway Analysis", className="navbar-section-title"),
-                        html.A("Gene Count Chart", href="#gene-count-chart", className="nav-link"),
-                        html.A("Violin Boxplot", href="#violin-boxplot", className="nav-link"),
-                        html.A("Pathway KO Bar Chart", href="#pathway-ko-bar-chart", className="nav-link"),
-                        html.A("Sample KO Pathway Chart", href="#sample-ko-pathway-chart", className="nav-link"),
-                        html.A("Scatter Plot of KOs by Sample", href="#sample-ko-scatter", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("2 - Gene and Metabolic Pathway Analysis", className="card-title"),
+                                    html.A("Gene Counts Across Samples", href="#gene-count-chart", className="nav-link"),
+                                    html.A("Gene Distribution Among Samples", href="#violin-boxplot", className="nav-link"),
+                                    html.A("Distribution of KO in Pathways", href="#pathway-ko-bar-chart", className="nav-link"),
+                                    html.A("Pathway Activity per Sample", href="#sample-ko-pathway-chart", className="nav-link"),
+                                    html.A("Scatter Plot of KOs by Sample", href="#sample-ko-scatter", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
 
                         # Sessão 3: Interactions Between Entities
-                        html.P("3 - Interactions Between Entities", className="navbar-section-title"),
-                        html.A("Samples vs Compounds", href="#compound-scatter-chart", className="nav-link"),
-                        html.A("Genes vs Compounds", href="#gene-compound-scatter-chart", className="nav-link"),
-                        html.A("Samples vs Genes", href="#sample-gene-scatter-chart", className="nav-link"),
-                        html.A("Enzyme Activity per Sample", href="#sample-enzyme-activity", className="nav-link"),
-                        html.A("Gene-Compound Network", href="#gene-compound-network", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("3 - Interactions Between Entities", className="card-title"),
+                                    html.A("Sample-Compound Interaction", href="#compound-scatter-chart", className="nav-link"),
+                                    html.A("Gene-Compound Interaction", href="#gene-compound-scatter-chart", className="nav-link"),
+                                    html.A("Sample-Gene Associations", href="#sample-gene-scatter-chart", className="nav-link"),
+                                    html.A("Enzyme Activity by Sample", href="#sample-enzyme-activity", className="nav-link"),
+                                    html.A("Gene-Compound Network", href="#gene-compound-network", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
+
+                        # Sessão 4: Ranking and Prioritization
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("4 - Ranking and Prioritization", className="card-title"),
+                                    html.A("Ranking of Samples by Compound Interaction", href="#sample-rank-compounds-chart", className="nav-link"),
+                                    html.A("Ranking of Compounds by Sample Interaction", href="#compound-rank-chart", className="nav-link"),
+                                    html.A("Ranking of Compounds by Gene Interaction", href="#compound-rank-gene-chart", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
 
                         # Sessão 5: Patterns and Interactions with Heatmaps
-                        html.P("5 - Patterns and Interactions with Heatmaps", className="navbar-section-title"),
-                        html.A("Samples vs Reference AG", href="#sample-reference-heatmap", className="nav-link"),
-                        html.A("Genes vs Samples Heatmap", href="#gene-sample-heatmap", className="nav-link"),
-                        html.A("Pathways vs Compound Pathways", href="#pathway-heatmap", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("5 - Patterns and Interactions with Heatmaps", className="card-title"),
+                                    html.A("Sample-Reference Agency Heatmap", href="#sample-reference-heatmap", className="nav-link"),
+                                    html.A("Gene-Sample Heatmap", href="#gene-sample-heatmap", className="nav-link"),
+                                    html.A("Pathway-Compound Interaction Map", href="#pathway-heatmap", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
 
                         # Sessão 6: Intersection and Group Exploration
-                        html.P("6 - Intersection and Group Exploration", className="navbar-section-title"),
-                        html.A("Sample Groups by Compound Class", href="#sample-groups-chart", className="nav-link"),
-                        html.A("Sample UpSet Plot", href="#sample-upset-plot", className="nav-link"),
-                        html.A("Sample Clustering Dendrogram", href="#sample-clustering-dendrogram", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("6 - Intersection and Group Exploration", className="card-title"),
+                                    html.A("Sample Grouping by Compound Class Pattern", href="#sample-groups-chart", className="nav-link"),
+                                    html.A("Intersection Analysis", href="#sample-upset-plot", className="nav-link"),
+                                    html.A("Clustering Dendrogram", href="#sample-clustering-dendrogram", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
 
                         # Sessão 7: Toxicity Predictions
-                        html.P("7 - Toxicity Predictions", className="navbar-section-title"),
-                        html.A("Comprehensive Toxicity Heatmap", href="#toxicity-heatmap-faceted", className="nav-link"),
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H5("7 - Toxicity Predictions", className="card-title"),
+                                    html.A("Comprehensive Toxicity Prediction Heatmap", href="#toxicity-heatmap-faceted", className="nav-link"),
+                                ]
+                            ),
+                            className="nav-card",
+                        ),
                     ],
-                    className="nav-links",  # Todos os links e títulos dentro de um único contêiner
+                    className="nav-links",  # Contêiner geral com todos os cards
                 ),
             ],
             className="navbar-menu-container",  # Contêiner geral do menu
