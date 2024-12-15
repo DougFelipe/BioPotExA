4a1fce87203751cd3b312481b4dc638cbb4ee911
DougFelipe <dougbti2022@gmail.com>
2024-12-14 21:25:35 -0300

    style(navbar): ajusta o estilo da navbar para os cards ficarem alinhados

--- components/navbar.py
+++ components/navbar.py
@@ -26,7 +26,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("1 - Data Tables and Database Integration", className="card-title"),
+                                    html.H5("Data Tables and Database Integration", className="card-title"),
                                     html.A("BioRemPP Results Table", href="#main-results-table", className="nav-link"),
                                     html.A("HADEG Results Table", href="#hadeg-results-table", className="nav-link"),
                                     html.A("ToxCSM Results Table", href="#toxcsm-results-table", className="nav-link"),
@@ -39,7 +39,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("2 - Gene and Metabolic Pathway Analysis", className="card-title"),
+                                    html.H5("Gene and Metabolic Pathway Analysis", className="card-title"),
                                     html.A("Gene Counts Across Samples", href="#gene-count-chart", className="nav-link"),
                                     html.A("Gene Distribution Among Samples", href="#violin-boxplot", className="nav-link"),
                                     html.A("Distribution of KO in Pathways", href="#pathway-ko-bar-chart", className="nav-link"),
@@ -54,7 +54,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("3 - Interactions Between Entities", className="card-title"),
+                                    html.H5("Interactions Between Entities", className="card-title"),
                                     html.A("Sample-Compound Interaction", href="#compound-scatter-chart", className="nav-link"),
                                     html.A("Gene-Compound Interaction", href="#gene-compound-scatter-chart", className="nav-link"),
                                     html.A("Sample-Gene Associations", href="#sample-gene-scatter-chart", className="nav-link"),
@@ -69,7 +69,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("4 - Ranking and Prioritization", className="card-title"),
+                                    html.H5("Ranking and Prioritization", className="card-title"),
                                     html.A("Ranking of Samples by Compound Interaction", href="#sample-rank-compounds-chart", className="nav-link"),
                                     html.A("Ranking of Compounds by Sample Interaction", href="#compound-rank-chart", className="nav-link"),
                                     html.A("Ranking of Compounds by Gene Interaction", href="#compound-rank-gene-chart", className="nav-link"),
@@ -82,7 +82,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("5 - Patterns and Interactions with Heatmaps", className="card-title"),
+                                    html.H5("Patterns and Interactions with Heatmaps", className="card-title"),
                                     html.A("Sample-Reference Agency Heatmap", href="#sample-reference-heatmap", className="nav-link"),
                                     html.A("Gene-Sample Heatmap", href="#gene-sample-heatmap", className="nav-link"),
                                     html.A("Pathway-Compound Interaction Map", href="#pathway-heatmap", className="nav-link"),
@@ -95,7 +95,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("6 - Intersection and Group Exploration", className="card-title"),
+                                    html.H5("Intersection and Group Exploration", className="card-title"),
                                     html.A("Sample Grouping by Compound Class Pattern", href="#sample-groups-chart", className="nav-link"),
                                     html.A("Intersection Analysis", href="#sample-upset-plot", className="nav-link"),
                                     html.A("Clustering Dendrogram", href="#sample-clustering-dendrogram", className="nav-link"),
@@ -108,7 +108,7 @@ navbar = html.Div(
                         dbc.Card(
                             dbc.CardBody(
                                 [
-                                    html.H5("7 - Toxicity Predictions", className="card-title"),
+                                    html.H5("Toxicity Predictions", className="card-title"),
                                     html.A("Comprehensive Toxicity Prediction Heatmap", href="#toxicity-heatmap-faceted", className="nav-link"),
                                 ]
                             ),
