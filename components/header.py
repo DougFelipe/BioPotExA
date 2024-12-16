3ccb4c6ec00b07cfeba82b6f22e3a8fb3ea2dafd
DougFelipe <dougbti2022@gmail.com>
2024-12-15 22:04:39 -0300

    Refatora layout da página de Features: - Remove componentes  e chamadas de  para simplificar a estrutura. - Mantém os textos, títulos e alertas exatamente como no layout original. - Substitui os layouts por placeholders simples, alinhando o estilo da página de Features ao novo padrão. - Organiza o código para melhor legibilidade e consistência

--- components/header.py
+++ components/header.py
@@ -8,7 +8,7 @@ def Header():
         ]),
         html.Div(className='header-right', children=[
             html.A("Help", href="/help", className="header-link"),
-            html.A('BioPExa Features', href='/features', className='header-link'),
+            html.A('Expected Results', href='/features', className='header-link'),
             html.A('Regulatory Agencies', href='/regulatory', className='header-link'),
             html.A('Bioremediation', href='/bioremediation', className='header-link'),
             #html.A('Changelog', href='/changelog', className='header-link'),
