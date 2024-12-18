cad3899c4bef1bd71765a225bb8af0384e7b77e8
DougFelipe <dougbti2022@gmail.com>
2024-12-18 20:08:33 -0300

    style(data_analysis e about): modifica conteudo de texto da página para ficar padronizado e explicativo

--- index.py
+++ index.py
@@ -116,4 +116,5 @@ def display_page(pathname):
 # ----------------------------------------
 
 if __name__ == '__main__':
-    app.run_server(debug=True)  # Start the Dash server
+    app.run_server(debug=True)  # Development
+    # app.run_server(debug=False, host="0.0.0.0", port="8050") #Production
