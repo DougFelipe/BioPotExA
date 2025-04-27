d3aa90ea3ee8e04b0eb83967c62f704d4c74285a
Douglas <dougbti2022@gmail.com>
2025-04-27 03:48:27 -0300

    feat: Update server configuration for production and add VSCode settings

--- index.py
+++ index.py
@@ -116,5 +116,5 @@ def display_page(pathname):
 # ----------------------------------------
 
 if __name__ == '__main__':
-    app.run_server(debug=True)  # Development
-    # app.run_server(debug=False, host="0.0.0.0", port="8050") #Production
+    app.run(debug=False, host="0.0.0.0", port=8050)
+    #app.run_server(debug=True)  # Development
